5411c133d72e13dd39b128ea2508cf5fecfa79e4
Tejaswi <lokesh1404@outlook.com>
2023-11-02 13:40:20 +0530

    E-commerce website

---
+++ initial/footer.js
@@ -0,0 +1,27 @@
+// Footer Injection
+document.write(
+    <footer>
+        <div class="footer-categories">
+            <div class="footer-category">
+                <h3>Women</h3>
+                {/* <!-- List subcategories here --> */}
+            </div>
+            <div class="footer-category">
+                <h3>Men</h3>
+                {/* <!-- List subcategories here --> */}
+            </div>
+            <div class="footer-category">
+                <h3>Kids</h3>
+                {/* <!-- List subcategories here --> */}
+            </div>
+            <div class="footer-category">
+                <h3>Links</h3>
+                <a href="home.html">Home</a>
+                <a href="contact.html">Contact</a>
+                <a href="login.html">Login</a>
+            </div>
+        </div>
+      
+        <p class="copyright">© 2023 Your E-commerce Website</p>
+    </footer>
+);
